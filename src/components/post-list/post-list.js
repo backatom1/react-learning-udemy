d5e60a40d9317e1c2af801b5bb895ce1ea7081e5
vv_tomilin <backatom1@gmail.com>
2021-05-02 11:17:49 +0500

    Lesson 92. Свойства и состояния компонентов. События в React

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -3,12 +3,25 @@ import PostListItem from '../post-list-item/';
 
 import './post-list.css';
 
-const PostList = () => {
+const PostList = ({ posts }) => {
+
+	const elements = posts.map((item) => {
+
+		const { id, label, important } = item;
+
+		return (
+			<li key={id} className='list-group-item'>
+				<PostListItem
+					label={label}
+					important={important}
+				/>
+			</li>
+		);
+	});
+
 	return (
 		<ul className='app-list list-group'>
-			<PostListItem />
-			<PostListItem />
-			<PostListItem />
+			{elements}
 		</ul>
 	);
 };
